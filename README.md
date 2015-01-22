2fda581609cd6d1dba3713153d2f608833a3060a
Prabha Matta <prabha.matta@gmail.com>
2015-01-22 00:38:30 -0800

    photo crud

--- README.md
+++ README.md
@@ -1,24 +1,26 @@
 # PhotoHub
 
+## Features
 
-### Ruby version
+
+## Ruby version
 - Ruby 2.0
 - Rails 4.1.5
 
-### Gems used
-- ```gem 'devise', '~> 3.3.0' ``` - for user signup, signin, signout, account edit
-- ``` gem 'bootstrap-sass', '~> 3.2.0.2' ``` - for styling
+## Gems used
+```
+  gem 'haml', '~> 4.0.6'
+  gem 'bootstrap-sass', '~> 3.3.3'
+  gem 'simple_form', '~> 3.1.0'
+```
 
-
-### Configuration & Database creation
+## Configuration & Database creation
 ```
 bundle install
 bundle exec rake db:migrate
 ```
 
-### How to run the test suite
-
-### Services (job queues, cache servers, search engines, etc.)
-
-### Deployment
+## Deployment
+```
 rails s
+```
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,48 @@
 class PhotosController < ApplicationController
+  before_action :find_photo, only: [:show, :edit, :update, :destroy]
+  def index
+    @photos = Photo.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      redirect_to @photo, notice: "Successfully created new Photo"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @photo.update(photo_params)
+      redirect_to @photo, notice: "Photo was Successfully updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @photo.destroy
+    redirect_to root_path
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:title, :description)
+  end
+
+  def find_photo
+    @photo = Photo.find(params[:id])
+  end
+
 end
